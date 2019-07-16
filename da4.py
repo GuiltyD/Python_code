8f7fc137f62186d3aaa6325a5a0bc99d86ce30c6
daishuhao <597445210@qq.com>
2019-07-16 22:58:31 +0800

    modify da4.py

--- da4.py
+++ da4.py
@@ -1,11 +1,3 @@
-from collections import Counter
-s = [
-        'look', 'into', 'my', 'eyes', 'look', 'into', 'my', 'eyes',
-        'the', 'eyes', 'the', 'eyes', 'the', 'eyes', 'not', 'around', 'the',
-        'eyes', "don't", 'look', 'around', 'the', 'eyes', 'look', 'into',
-        'my', 'eyes', "you're", 'under'
-        ]
-
 count = Counter(s)
 new_count = zip(count.values(),count.keys())
 
--- partialtest.py
+++
@@ -1,8 +0,0 @@
-from functools import partial
-
-def demo(x,y):
-    return x-y
-x_list = [1,5,8,8,5,87,3]
-sort_list = sorted(x_list,key =partial(demo,y=32))
-print(sort_list)
-print('hello world')
